4aa6d39066c0166584c13c00dc0809c84f65ead7
Girinath <f2016018@pilani.bits-pilani.ac.in>
2018-11-14 02:23:32 +0530

    Update QAlive2.py

--- QAlive2.py
+++ QAlive2.py
@@ -251,7 +251,7 @@ def live(model, input_query, vocabulary, length_preprocessed, inverted_index, do
 	nb_documents = len(doc_index) 	
 	user_document_array = np.zeros(nb_documents)
 	doc_ids = doc_index.values()
-	rows = json.load(urllib.urlopen("127.0.0.1:8000/clicks/" + str(user_id) + '/')['clicks'];
+	rows = json.load(urllib.urlopen("http://127.0.0.1:8000/clicks/" + str(user_id) + '/'))['clicks'];
 	for row in rows:
 		user_document_array[doc_ids.index(str(row[0]))] = row[1]
 	user_document_array = torch.FloatTensor(user_document_array)
